11f73baeb959fe4a1ab1783859e3742fbb7ec05e
enverpasha0194 <enverpasaninfesi@gmail.com>
2025-12-18 23:12:12 +0300

    Update scriber_web.py

--- scriber_web.py
+++ scriber_web.py
@@ -8,7 +8,7 @@ import bcrypt
 # 🔑 AYARLAR
 # ==============================
 SUPABASE_URL = "https://rhenrzjfkiefhzfkkwgv.supabase.co"
-SUPABASE_KEY = "ANON_KEYİN"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoZW5yempma2llZmh6Zmtrd2d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNzY3MTMsImV4cCI6MjA4MTY1MjcxM30.gwjvIT5M8PyP9SBysXImyNblPm6XNwJTeZAayUeVCxU"
 NGROK_URL = "https://hydropathical-duodecastyle-camron.ngrok-free.dev"
 LOGO_URL = "https://i.ibb.co/CD44FDc/Chat-GPT-mage-17-Ara-2025-23-59-13.png"
 
@@ -202,3 +202,4 @@ if prompt := st.chat_input("Scriber'a yaz..."):
         st.markdown(reply)
 
     st.session_state.history.append({"role":"assistant","content":reply})
+
