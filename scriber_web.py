dc6049e35f3cd747a283889a8822192d9cf2695f
enverpasha0194 <enverpasaninfesi@gmail.com>
2025-12-23 20:29:25 +0300

    Update scriber_web.py

--- scriber_web.py
+++ scriber_web.py
@@ -1,69 +1,59 @@
+import streamlit as st
+import streamlit.components.v1 as components
 
-
+html_code = """
 <!DOCTYPE html>
 <html lang="tr">
 <head>
     <meta charset="UTF-8">
-    <title>Uygulama Çalışma Altında</title>
     <style>
-        * {
-            box-sizing: border-box;
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
         body {
             margin: 0;
-            padding: 0;
             width: 100vw;
             height: 100vh;
-            background-color: #ffffff;
+            background-color: white;
             display: flex;
             justify-content: center;
             align-items: center;
+            font-family: Arial, Helvetica, sans-serif;
         }
 
         .container {
             text-align: center;
         }
 
-        .container img {
+        img {
             width: 180px;
-            height: auto;
             margin-bottom: 30px;
         }
 
-        .title {
+        h1 {
             font-size: 36px;
-            font-weight: 800;
             letter-spacing: 2px;
-            color: #000;
             margin-bottom: 15px;
         }
 
-        .description {
+        p {
             font-size: 15px;
             color: #555;
             max-width: 420px;
-            margin: 0 auto;
+            margin: auto;
             line-height: 1.6;
-        }
-
-        .description span {
-            color: #000;
-            font-weight: 600;
         }
     </style>
 </head>
 <body>
     <div class="container">
-        <img src="https://media.tenor.com/K5wSW-CGK9wAAAAi/maintenance-under-maintenance.gif" alt="Maintenance">
-        <div class="title">UYGULAMA ÇALIŞMA ALTINDA!</div>
-        <div class="description">
-            Şu anda sistemin vidaları sıkılıyor, kablolar düzeltiliyor.<br>
-            <span>Performans, güvenlik ve birkaç sürpriz</span> için kısa bir mola verdik.
-            <br><br>
-            Birazdan buradayız. Kahveni kap gel ☕
-        </div>
+        <img src="https://media.tenor.com/K5wSW-CGK9wAAAAi/maintenance-under-maintenance.gif">
+        <h1>UYGULAMA ÇALIŞMA ALTINDA!</h1>
+        <p>
+            Sistem şu an güncelleme modunda.<br>
+            Performans artışı, stabilite ve birkaç gizli dokunuş yapılıyor.<br><br>
+            Birazdan buradayız, Kahveni al gel. ☕
+        </p>
     </div>
 </body>
 </html>
+"""
+
+components.html(html_code, height=600)
